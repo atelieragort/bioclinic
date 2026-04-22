7a91f96b2f0a0225aac6cbb8b2bd95095b53451b
Billrider9 <BigWalksHh@hotmail.com>
2026-04-22 19:20:45 +0300

    fix layout.tsx, feat page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,3 +14,22 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+export const metadata: Metadata = {
+  title: "Healthcare",
+  description: "Preventative healthcare, such as annual check-ups and vaccinations, saves lives and money in the long run.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0B1120] min-h-screen`}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
